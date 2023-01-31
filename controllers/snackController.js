33d28a026546c817d40f4dea225b0703ad599ee3
Bo Latt <bolattny@gmail.com>
2023-01-31 00:34:16 -0500

    move sql queries from controller to queries folder

--- controllers/snackController.js
+++ controllers/snackController.js
@@ -1,17 +1,37 @@
 const express = require("express");
 const snacks = express.Router();
+const { getAllSnacks, getOneSnack } = require("../queries/snacks");
 
-const db = require("../db/dbConfig");
+// const db = require("../db/dbConfig");
+
+// snacks.get("/", async (req, res) => {
+//   const snacksList = await db.any("SELECT * FROM snacks");
+//   res.send(snacksList);
+// });
+
+// snacks.get("/:id", async (req, res) => {
+//   const id = req.params.id;
+//   const snack = await db.oneOrNone("SELECT * FROM snacks WHERE id = $1", id);
+//   res.json(snack);
+// });
 
 snacks.get("/", async (req, res) => {
-  const snacksList = await db.any("SELECT * FROM snacks");
-  res.send(snacksList);
+  try {
+    const snacks = await getAllSnacks();
+    res.json(snacks);
+  } catch (error) {
+    res.status(400).json({ error: "something went wrong!" });
+  }
 });
 
 snacks.get("/:id", async (req, res) => {
-  const id = req.params.id;
-  const snack = await db.oneOrNone(`SELECT * FROM snacks WHERE id = $1`, id);
-  res.json(snack);
+  try {
+    const { id } = req.params;
+    const snack = await getOneSnack(id);
+    res.json(snack);
+  } catch (error) {
+    res.status(404).json({ error: "That Snack doesn't exist!!" });
+  }
 });
 
 module.exports = snacks;
