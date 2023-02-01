c2da4e2c775aff12c7e1dfe4db9bc48742a7432f
Bo Latt <bolattny@gmail.com>
2023-01-31 19:49:50 -0500

    add delete query and route

--- queries/snacks.js
+++ queries/snacks.js
@@ -5,4 +5,7 @@ const getAllSnacks = async () => await db.any("SELECT * FROM snacks");
 const getOneSnack = async (id) =>
   await db.one("SELECT * FROM snacks WHERE id=$1", id);
 
-module.exports = { getAllSnacks, getOneSnack };
+const deleteSnack = async (id) =>
+  await db.one("DELETE FROM snacks WHERE id = $1 RETURNING *", id);
+
+module.exports = { getAllSnacks, getOneSnack, deleteSnack };
